c6ab448894a76131a64997f12e2e459d99592810
Putu Widyantara <putuwaw973@gmail.com>
2023-02-09 00:04:04 +0800

    Configure routes for bot webhook

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,9 +1,24 @@
-from flask import render_template
+from flask import request, render_template
 from modules import modules
+import telebot
+import time
+import os
 
-def configure_routes(app):
+
+def configure_routes(app, bot):
     @app.route("/")
     def index():
+        bot.remove_webhook()
+        time.sleep(1)
+        bot.set_webhook(url=os.getenv("URL"))
+
         hello = modules.hello()
         content = modules.content()
         return render_template("index.html", hello=hello, content=content)
+
+    @app.route('/webhook', methods=['POST'])
+    def webhook():
+        update = telebot.types.Update.de_json(
+            request.stream.read().decode("utf-8"))
+        bot.process_new_updates([update])
+        return "ok", 200
